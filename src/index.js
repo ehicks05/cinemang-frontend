81bd5b6a5366756226e26de04ff0e0ac3024b9c0
Eric Hicks <ehicks05@yahoo.com>
2021-09-02 23:21:22 -0400

    remove gql stuff

--- src/index.js
+++ src/index.js
@@ -1,68 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import { Provider } from "react-supabase";
 import { BrowserRouter } from "react-router-dom";
-import { ModalProvider } from "react-modal-hook";
-import { Auth } from "@supabase/ui";
 
 import { supabase } from "./supabase";
 import App from "./App";
 import "./index.css";
 
-const authClient = supabase.auth;
-
-const uri =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_PROD_GRAPHQL_URI
-    : process.env.REACT_APP_DEV_GRAPHQL_URI;
-
-/* Set URI for all Apollo GraphQL requests (backend api) */
-const httpLink = new HttpLink({
-  uri,
-  fetchOptions: { credentials: "same-origin" },
-});
-
-/* Create Apollo Link to supply token */
-const withTokenLink = setContext(() => {
-  return { authToken: authClient.session()?.access_token };
-});
-
-/* Create Apollo Link to supply token in auth header with every gql request */
-const authLink = setContext((_, { headers, authToken }) => ({
-  headers: {
-    ...headers,
-    ...(authToken ? { authorization: `Bearer ${authToken}` } : {}),
-  },
-}));
-
-/* Create Apollo Client */
-const client = new ApolloClient({
-  link: ApolloLink.from([withTokenLink, authLink, httpLink]),
-  cache: new InMemoryCache({}),
-});
-
 /* Create root render function */
 const renderApp = (Component) => {
   render(
-    <Auth.UserContextProvider supabaseClient={supabase}>
-      <ApolloProvider client={client}>
-        <Provider value={supabase}>
-          <BrowserRouter>
-            <ModalProvider>
-              <App />
-            </ModalProvider>
-          </BrowserRouter>
-        </Provider>
-      </ApolloProvider>
-    </Auth.UserContextProvider>, 
+    <Provider value={supabase}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
   );
 };
--- src/useUser.js
+++
@@ -1,9 +0,0 @@
-import { Auth } from "@supabase/ui";
-
-const useUser = () => {
-  const { session, user } = Auth.useUser();
-
-  return { session, user, username: user?.app_metadata?.username };
-};
-
-export default useUser;
