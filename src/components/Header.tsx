1e73833427fb30eb77c2e5aaa01e3f561c4962d0
Eric Hicks <ehicks05@yahoo.com>
2021-09-11 15:46:05 -0400

    give urbanist font a try for the site title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import { FaCat } from "react-icons/all";
 const Header = () => {
   return (
     <div className="max-w-screen-lg mx-auto">
-      <div className="flex items-center gap-1 p-4 text-2xl">
+      <div
+        className="flex items-center gap-1 p-4 text-2xl"
+        style={{ fontFamily: "Urbanist" }}
+      >
         Cinemang
         <FaCat className="inline text-green-500" />
       </div>
