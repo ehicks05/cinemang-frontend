fa8da704ae4172a9c1cd7e506e6159235c6fb763
Eric Hicks <ehicks05@yahoo.com>
2021-09-16 00:06:51 -0400

    extract Film

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,19 +1,13 @@
 import React, { FC, useEffect, useState } from "react";
 import { Loading } from "components";
 import { useClient } from "react-supabase";
-import { addMinutes, intervalToDuration, parseISO } from "date-fns";
-import { usePalette } from "react-palette";
-import { truncate } from "lodash";
 import SearchForm, {
   ISearchForm,
   DEFAULT_SEARCH_FORM,
 } from "./components/SearchForm";
-import WatchProviders from "./components/WatchProviders";
-import { format } from "date-fns";
-import chroma from "chroma-js";
 import { useDebounce } from "react-use";
 import { Paginator } from "./components";
-import Stats from "./components/Stats";
+import Film from "./components/Film";
 
 const PAGE_SIZE = 20;
 
@@ -187,97 +181,6 @@ const Films: FC<{
         })}
       </div>
     </>
-  );
-};
-
-const Film = ({
-  film,
-  genres,
-  languages,
-}: {
-  film: any;
-  genres: any[];
-  languages: any[];
-}) => {
-  const findLanguage = (languageId: string) => {
-    return languages?.find((lang) => lang.id === languageId);
-  };
-
-  const findGenre = (genreId: string) => {
-    return genres?.find((genre) => genre.id === genreId);
-  };
-
-  const getGenreName = (genre: { name: string }) => {
-    const CUSTOM_NAMES = { "Science Fiction": "Sci-Fi" } as Record<
-      string,
-      string
-    >;
-    return CUSTOM_NAMES[genre.name] || genre.name;
-  };
-
-  const posterPath = film.poster_path
-    ? `https://image.tmdb.org/t/p/w92/${film.poster_path}`
-    : "/92x138.png";
-  const releasedAt = format(parseISO(film.released_at), "MM-dd-yyyy");
-  const year = format(parseISO(film.released_at), "yyyy");
-  const runtime = intervalToDuration({
-    start: new Date(),
-    end: addMinutes(new Date(), Number(film.runtime)),
-  });
-
-  const { data: palette, loading, error } = usePalette(posterPath);
-  const [truncateOverview, setTruncateOverview] = useState(true);
-
-  if (loading || error) return <Loading error={error} loading={loading} />;
-
-  const lessMuted = chroma.mix(palette.darkVibrant || "", "rgb(38,38,38)", 0.7);
-  const muted = chroma.mix(palette.darkVibrant || "", "rgb(38,38,38)", 0.95);
-  const cardStyle = {
-    background: `linear-gradient(45deg, ${muted} 5%, ${muted} 45%, ${lessMuted} 95%)`,
-  };
-
-  const statData = {
-    voteAverage: film.vote_average,
-    voteCount:
-      Number(film.vote_count) > 1000
-        ? `${Math.round(film.vote_count / 1000)}k`
-        : film.vote_count,
-    language: findLanguage(film.language_id).name,
-    genre: getGenreName(findGenre(film.genre_id)),
-  };
-
-  return (
-    <div className="flex flex-col gap-4 p-4 rounded-lg" style={cardStyle}>
-      <div className="flex gap-4">
-        <div className="flex-shrink-0">
-          <img className="" src={posterPath} alt="poster" />
-        </div>
-        <div className="flex flex-col gap-1">
-          <div>
-            <span className="font-bold text-lg">{film.title}</span>{" "}
-            <span className="text-xs text-gray-300" title={releasedAt}>
-              ({year})
-            </span>
-          </div>
-          <div className="text-xs text-gray-300">{`${runtime.hours}h ${runtime.minutes}m`}</div>
-          <div>{film.director}</div>
-          <div>{film.cast}</div>
-          <WatchProviders watchProviders={film.watch_providers} />
-        </div>
-      </div>
-      <div className="flex flex-col gap-4">
-        <Stats bgColor={palette.darkVibrant || ""} data={statData} />
-        <div
-          className="text-justify"
-          onClick={() => setTruncateOverview(!truncateOverview)}
-        >
-          {truncate(film.overview, {
-            length: truncateOverview ? 256 : 1024,
-            separator: " ",
-          })}
-        </div>
-      </div>
-    </div>
   );
 };
 
