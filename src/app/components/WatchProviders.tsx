ad632fe6537f530cb8594bef3ce987d367f0142d
Eric Hicks <ehicks05@yahoo.com>
2021-09-18 03:49:17 -0400

    filter providers by display_priority

--- src/app/components/WatchProviders.tsx
+++ src/app/components/WatchProviders.tsx
@@ -4,12 +4,12 @@ import { SiAmazon, SiAppletv, SiHulu, SiNetflix } from "react-icons/si";
 
 interface Provider {
   id?: number;
-  name?: string;
+  provider_name?: string;
   Icon?: IconType;
   color?: string;
 }
 
-const PROVIDERS: Record<number, Provider> = {
+const PROVIDER_RENDER_DETAILS: Record<number, Provider> = {
   8: { Icon: SiNetflix, color: "text-netflix" },
   9: { Icon: SiAmazon, color: "text-amazon" },
   15: { Icon: SiHulu, color: "text-green-500" },
@@ -22,27 +22,31 @@ interface WatchProviderProps {
 }
 
 const WatchProvider: FC<WatchProviderProps> = ({
-  provider: { Icon, name, color = "text-gray-400" },
+  provider: { Icon, provider_name, color = "text-gray-400" },
 }) => {
   return (
     <div className="flex p-1 text-gree rounded border border-solid border-gray-600">
       <div className="flex">
         {Icon && <Icon className={`${color} text-xl`} />}
-        {!Icon && <span className={`${color} text-xs`}>{name}</span>}
+        {!Icon && <span className={`${color} text-xs`}>{provider_name}</span>}
       </div>
     </div>
   );
 };
 
 interface Props {
-  watchProviders: { provider_id: number; provider_name: string }[];
+  watchProviders: {
+    provider_id: number;
+    provider_name: string;
+    display_priority: number;
+  }[];
 }
 
 const WatchProviders: FC<Props> = ({ watchProviders }) => {
   if (!watchProviders) return null;
   const providers = watchProviders
-    .map((p) => ({ ...PROVIDERS[p.provider_id], name: p.provider_name }))
-    .filter((p) => p);
+    .map((p) => ({ ...p, ...PROVIDER_RENDER_DETAILS[p.provider_id] }))
+    .filter((p) => p.display_priority <= 6);
 
   if (providers.length === 0) return null;
   return (
