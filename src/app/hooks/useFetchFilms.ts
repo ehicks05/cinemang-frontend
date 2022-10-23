1ff612d058edee2229ead72ce20eed1bd56c8548
Eric Hicks <ehicks05@yahoo.com>
2022-10-23 14:19:06 -0400

    refactor

--- src/app/hooks/useFetchFilms.ts
+++ src/app/hooks/useFetchFilms.ts
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useDebounce } from 'react-use';
-import { useQueryParams } from 'use-query-params';
+import {
+  DecodedValueMap,
+  QueryParamConfigMap,
+  useQueryParams,
+} from 'use-query-params';
 import { PAGE_SIZE } from '../../constants';
 import { supabase } from '../../supabase';
 
@@ -9,9 +13,68 @@ interface Data {
   films: any[] | null;
 }
 
+const idQuery = async (
+  form: DecodedValueMap<QueryParamConfigMap>,
+  page: number,
+) => {
+  const query = supabase
+    .from('movie')
+    .select(
+      `id${
+        form.watchProviders.length > 0 ? ', wp: watch_provider!inner(id)' : ''
+      }`,
+      {
+        count: 'exact',
+      },
+    )
+    .ilike('title', `%${form.title || ''}%`)
+    .gte('vote_count', form.minVotes || 0)
+    .lte('vote_count', form.maxVotes || 100_000_000)
+    .gte('vote_average', form.minRating || 0)
+    .lte('vote_average', form.maxRating || 10)
+    .gte('released_at', form.minReleasedAt || '1870-01-01')
+    .lte('released_at', form.maxReleasedAt || new Date().toLocaleDateString())
+    .like('language_id', form.language || '*');
+
+  if (form.genre) {
+    query.eq('genre_id', form.genre || '*');
+  }
+
+  if (form.watchProviders.length > 0) {
+    query.in('wp.id', form.watchProviders);
+  }
+
+  query
+    .order(form.sortColumn, { ascending: form.ascending })
+    .order('id', { ascending: true })
+    .range(page * PAGE_SIZE, (page + 1) * PAGE_SIZE - 1);
+
+  return query;
+};
+
+const hydrationQuery = async (
+  ids: number[],
+  form: DecodedValueMap<QueryParamConfigMap>,
+) => {
+  const select = [
+    '*',
+    'watch_provider(id)',
+    'cast_credit(*, person(*))',
+    'crew_credit(*, person(*))',
+  ].join(',');
+
+  return supabase
+    .from('movie')
+    .select(select)
+    .in('id', ids)
+    .order(form.sortColumn, { ascending: form.ascending })
+    .order('id', { ascending: true });
+};
+
 export const useFetchFilms = ({ page }: { page: number }) => {
   const [formParams] = useQueryParams();
 
+  // a local, debounced copy of the form
   const [form, setForm] = useState(formParams);
   const [data, setData] = useState<Data>({ count: 0, films: [] });
   const [loading, setLoading] = useState(true);
@@ -30,55 +93,14 @@ export const useFetchFilms = ({ page }: { page: number }) => {
 
     const fetchFilms = async () => {
       try {
-        const query = supabase
-          .from('movie')
-          .select(
-            `id${
-              form.watchProviders.length > 0
-                ? ', wp: watch_provider!inner(id)'
-                : ''
-            }`,
-            {
-              count: 'exact',
-            },
-          )
-          .ilike('title', `%${form.title || ''}%`)
-          .gte('vote_count', form.minVotes || 0)
-          .lte('vote_count', form.maxVotes || 100_000_000)
-          .gte('vote_average', form.minRating || 0)
-          .lte('vote_average', form.maxRating || 10)
-          .gte('released_at', form.minReleasedAt || '1870-01-01')
-          .lte(
-            'released_at',
-            form.maxReleasedAt || new Date().toLocaleDateString(),
-          )
-          .like('language_id', form.language || '*');
+        // query 1: identify the ids for our search results
+        const { data, count } = await idQuery(form, page);
+        const ids = data?.map((row) => row.id) || [];
 
-        if (form.genre) {
-          query.eq('genre_id', form.genre || '*');
-        }
+        // query 2: fetch data for the ids
+        const { data: films } = await hydrationQuery(ids, form);
 
-        if (form.watchProviders.length > 0) {
-          query.in('wp.id', form.watchProviders);
-        }
-
-        query
-          .order(form.sortColumn, { ascending: form.ascending })
-          .order('id', { ascending: true })
-          .range(page * PAGE_SIZE, (page + 1) * PAGE_SIZE - 1);
-
-        const { data, count } = await query;
-
-        const hydrationQuery = supabase
-          .from('movie')
-          .select('*, watch_provider(id)')
-          .in('id', data?.map((row) => row.id) || [])
-          .order(form.sortColumn, { ascending: form.ascending })
-          .order('id', { ascending: true });
-
-        const { data: films } = await hydrationQuery;
-
-        await setData({ count: count || 0, films });
+        setData({ count: count || 0, films });
       } catch (e) {
         setError(e);
       } finally {
