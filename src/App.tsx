233f90ff70677059a67e658ad2ddc2672632236e
Eric Hicks <ehicks05@yahoo.com>
2022-10-25 23:20:32 -0400

    padding

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-tr from-indigo-900 to-green-900 text-gray-50">
       <Header />
-      <div className="flex h-full flex-grow flex-col p-4">
+      <div className="flex h-full flex-grow flex-col p-4 pt-0">
         <Routes>
           <Route element={<Home />} path={'/'} />
           <Route element={<FilmDetail />} path={'/films/:id'} />
