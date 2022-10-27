36059076538c976474a57d279aafc440908b06cf
Eric Hicks <ehicks05@yahoo.com>
2022-10-26 22:31:31 -0400

    adjust

--- src/core-components/Loading.tsx
+++ src/core-components/Loading.tsx
@@ -2,22 +2,21 @@ import { FC } from 'react';
 import { VscLoading } from 'react-icons/vsc';
 
 interface Props {
-  error: any;
+  error?: any;
   loading: boolean;
   size?: number;
 }
 
 const Loading: FC<Props> = ({ loading, error, size = 128 }) => {
+  if (error) {
+    return <span>Error: {JSON.stringify(error, null, 2)}</span>;
+  }
   if (loading) {
     return (
       <div className="flex flex-grow items-center justify-center">
         <VscLoading className="animate-spin" color="#15eda1" size={size} />
       </div>
     );
-  }
-
-  if (error) {
-    return <span>Error: {JSON.stringify(error, null, 2)}</span>;
   }
 
   return null;
