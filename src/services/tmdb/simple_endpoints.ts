d4c18aecac2cfb00c52208bc2c6fd33365de3f86
Eric Hicks <ehicks05@yahoo.com>
2022-11-04 17:45:09 -0400

    404 shh

--- src/services/tmdb/simple_endpoints.ts
+++ src/services/tmdb/simple_endpoints.ts
@@ -16,7 +16,8 @@ export const logAxiosError = (error: AxiosError) => {
     // The request was made and the server responded with a status code
     // that falls out of the range of 2xx
     const { data, status, headers } = error.response;
-    logger.error({ data, status, config /* headers */ });
+    if (status === 404) logger.error({ status, url: config.url });
+    else logger.error({ data, status, config /* headers */ });
   } else if (error.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
