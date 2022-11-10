0046ebf9070f83d6faa4763e5d45556b54879052
Eric Hicks <ehicks05@yahoo.com>
2022-11-09 21:01:40 -0500

    remove unused cache, redis

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,11 @@ require('dotenv').config();
 import updateDb from './app/tmdb_loader';
 import { scheduleUpdateTask } from './tasks';
 import logger from './services/logger';
-import cache from './services/cache';
 
 const db_url = process.env.DATABASE_URL;
 if (db_url) logger.info(`DB: ${db_url.slice(db_url.indexOf('@') + 1)}`);
 
 const init = async () => {
-  cache.create('MOVIE');
-  cache.create('PERSON');
-
   updateDb();
   scheduleUpdateTask();
 };
