ed0968e3497a60e83de578366974c8bf00a2fc95
Eric Hicks <ehicks05@yahoo.com>
2023-04-21 09:53:28 -0400

    absolute import support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const path = require('path');
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
