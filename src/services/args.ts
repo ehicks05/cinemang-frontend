56e95e16e26725f95356aa169bd8556c172db2f9
Eric Hicks <ehicks05@yahoo.com>
2023-08-05 16:40:49 -0400

    add --s alias

--- src/services/args.ts
+++ src/services/args.ts
@@ -3,7 +3,7 @@ import yargs from 'yargs';
 const argv = yargs(process.argv.slice(2))
   .options({
     full: { type: 'string', default: 'auto', choices: ['auto', 'off', 'on'] },
-    syncOnStart: { type: 'boolean', default: false },
+    syncOnStart: { type: 'boolean', default: false, alias: 's' },
   })
   .parseSync();
 
