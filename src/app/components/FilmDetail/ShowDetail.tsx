b51f1062edfc94608eb96d76c81f2f9b3bae22da
Eric Hicks <ehicks05@yahoo.com>
2023-08-08 21:07:53 -0400

    simple dump of season/episode data

--- src/app/components/FilmDetail/ShowDetail.tsx
+++ src/app/components/FilmDetail/ShowDetail.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { parseISO, format } from 'date-fns';
 import { useParams } from 'react-router-dom';
 import { useAtom } from 'jotai';
 import { useTitle } from 'react-use';
+import { FaCalendar, FaClock, FaHeart, FaStar } from 'react-icons/fa';
 import { Loading, OriginalImageLink } from '../../../core-components';
 import { usePalette } from '@/hooks/usePalette';
 import FilmStats from '../MediaStats';
 import MediaProviders from '../MediaProviders';
-import { Show } from '../../../types';
+import { Season, Show } from '../../../types';
 import { useFetchShow } from '@/hooks/useFetchShows';
 import Trailer from '../../../core-components/Trailer';
 import Credits from '../../../core-components/Credits';
 import { systemDataAtom } from '../../../atoms';
 import { getTmdbImage } from '../../../utils';
 import { toStats } from '../utils';
+import Stat from '../Stat';
 
 const ShowDetail = ({ show }: { show: Show }) => {
   useTitle(show.name, { restoreOnUnmount: true });
@@ -72,11 +74,88 @@ const ShowDetail = ({ show }: { show: Show }) => {
       </div>
 
       <pre className="text-xs">{JSON.stringify(show.seasons, null, 2)}</pre>
+      <Seasons seasons={show.seasons} />
 
       <Credits credits={show.credits} palette={palette} />
     </div>
   );
 };
+
+const SeasonCard = ({ season }: { season: Season }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div className="flex w-full flex-col gap-2 rounded-lg bg-neutral-800 p-4">
+      <div className="flex justify-between gap-2">
+        <span className="font-semibold">
+          {season.season_number}. {season.name}
+        </span>
+        <span className="flex items-center gap-2">
+          <Stat
+            icon={FaCalendar}
+            label={season.air_date}
+            bgColor="#333"
+            color="text-green-700"
+          />{' '}
+          <Stat
+            icon={FaHeart}
+            label={season.vote_average}
+            bgColor="#333"
+            color="text-red-600"
+          />
+        </span>
+      </div>
+      <div>{season.overview}</div>
+      <div className="flex flex-col gap-4">
+        {season.episodes.map(episode => (
+          <div className="flex flex-col gap-1 bg-neutral-700 p-2">
+            <div className="flex items-center justify-between gap-2">
+              <span>
+                {episode.episode_number}. {episode.name}
+              </span>
+              <span className="flex  gap-2">
+                <Stat
+                  icon={FaCalendar}
+                  label={episode.air_date}
+                  bgColor="#333"
+                  color="text-green-700"
+                />
+                <Stat
+                  icon={FaClock}
+                  label={episode.runtime || '?'}
+                  bgColor="#333"
+                  color="text-blue-500"
+                />
+                <Stat
+                  icon={FaHeart}
+                  label={episode.vote_average.toFixed(1)}
+                  bgColor="#333"
+                  color="text-red-600"
+                />
+                <Stat
+                  icon={FaStar}
+                  label={episode.vote_count}
+                  bgColor="#333"
+                  color="text-yellow-500"
+                />
+              </span>
+            </div>
+            <div>{episode.overview}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Seasons = ({ seasons }: { seasons: Season[] }) => (
+  <div className="flex flex-col gap-4">
+    <div className="text-xl font-bold">Seasons</div>
+    {seasons.map(season => (
+      <SeasonCard season={season} />
+    ))}
+  </div>
+);
 
 const Wrapper = () => {
   const { id } = useParams();
