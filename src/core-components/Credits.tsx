b342a63305d49677e25c805fc9998c7fe585e950
Eric Hicks <ehicks05@yahoo.com>
2023-08-13 14:19:38 -0400

    lint

--- src/core-components/Credits.tsx
+++ src/core-components/Credits.tsx
@@ -20,10 +20,10 @@ const Credits = ({ credits, palette }: Props) => {
       <h1 className="text-xl font-bold">Cast</h1>
       <div className="grid grid-cols-2 justify-center gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {cast
-          .sort((c1, c2) => c1.order - c2.order)
+          .sort((c1, c2) => (c1.order || 0) - (c2.order || 0))
           .map(c => (
             <PersonCard
-              character={c.character}
+              character={c.character || ''}
               key={c.credit_id}
               name={c.person.name}
               palette={palette}
@@ -38,7 +38,7 @@ const Credits = ({ credits, palette }: Props) => {
           .sort((c1, c2) => c2[0].person.popularity - c1[0].person.popularity)
           .map(c => (
             <PersonCard
-              jobs={c.map(c => c.job)}
+              jobs={c.map(c => c.job || '')}
               key={c[0].credit_id}
               name={c[0].person.name}
               palette={palette}
