8ef1ff9f898f81d5881bd9a0288cb8154cd1c693
Eric Hicks <ehicks05@yahoo.com>
2024-05-29 17:28:16 -0400

    remove 'valid ids' and 'recently changed ids' logic

--- src/services/tmdb/index.ts
+++ src/services/tmdb/index.ts
@@ -1,3 +1,2 @@
 export * from './simple_endpoints';
-export * from './valid_ids';
 export * from './discover';
--- src/services/tmdb/types.ts
+++
@@ -1,3 +0,0 @@
-import { RESOURCES } from './constants';
-
-export type ResourceKey = keyof typeof RESOURCES;
