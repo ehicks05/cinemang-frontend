22eba2c6311a93c4c5078c4244311e68f724bf3c
Eric Hicks <ehicks05@yahoo.com>
2024-05-29 18:33:30 -0400

    cleanup

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    '@ehicks05/eslint-config/packages/base',
-    '@ehicks05/eslint-config/packages/typescript',
-  ],
-};
