2f09ea4db47ec21d035383a3dbaf42df580ff984
Eric Hicks <ehicks05@yahoo.com>
2024-05-30 16:45:01 -0400

    lint

--- src/app/helpers/parse_movie.ts
+++ src/app/helpers/parse_movie.ts
@@ -2,37 +2,33 @@ import { Prisma } from '@prisma/client';
 import { pick } from 'lodash';
 import { MovieResponse } from '../../services/tmdb/types/responses';
 
-const MIN_VOTES = 64;
-
-export const isValidMovie = (movie: MovieResponse) =>
-	!!(
-		movie.credits &&
-		movie.credits.crew.find((c) => c.job === 'Director')?.name?.length &&
-		movie.credits.cast
-			.slice(0, 3)
-			.map((c) => c.name)
-			.join(', ')?.length &&
-		movie.genres[0] &&
-		movie.imdb_id &&
-		movie.overview &&
-		movie.poster_path &&
-		movie.release_date &&
-		movie.releases &&
-		movie.runtime &&
-		movie.vote_count >= MIN_VOTES
-	);
+const parseTopNCredits = (credits: MovieResponse['credits']['cast'], n = 3) =>
+	credits
+		.slice(0, n)
+		.map((c) => c.name)
+		.join(', ');
 
 export const parseMovie = (data: MovieResponse) => {
 	// ignore movies missing required data
-	if (!isValidMovie(data)) {
+	if (
+		!(
+			data.credits?.crew.find((c) => c.job === 'Director')?.name?.length &&
+			parseTopNCredits(data.credits.cast)?.length &&
+			data.genres[0] &&
+			data.imdb_id &&
+			data.overview &&
+			data.poster_path &&
+			data.release_date &&
+			data.releases &&
+			data.runtime &&
+			data.vote_count >= 64
+		)
+	) {
 		return undefined;
 	}
 
-	const director = data.credits.crew.find((c) => c.job === 'Director')?.name;
-	const cast = data.credits.cast
-		.slice(0, 3)
-		.map((c) => c.name)
-		.join(', ');
+	const director = data.credits.crew.find((c) => c.job === 'Director')?.name || '';
+	const cast = parseTopNCredits(data.credits.cast);
 	const certification =
 		data.releases.countries.find((r) => r.iso_3166_1 === 'US' && r.certification)
 			?.certification || '';
@@ -41,13 +37,12 @@ export const parseMovie = (data: MovieResponse) => {
 	const create: Prisma.MovieCreateInput = {
 		...pick(data, ['id', 'popularity', 'title']),
 		...{ cast, certification, director, genreId },
-		director: director!,
 		imdbId: data.imdb_id as string,
 		releasedAt: new Date(data.release_date),
 		languageId: data.original_language,
-		overview: data.overview!,
-		posterPath: data.poster_path!,
-		runtime: data.runtime!,
+		overview: data.overview,
+		posterPath: data.poster_path,
+		runtime: data.runtime,
 		voteCount: data.vote_count,
 		voteAverage: data.vote_average,
 	};
