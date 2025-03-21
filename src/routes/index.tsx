174d487f39ed5d783ec3c50bb995d30a2c521210
Eric Hicks <ehicks05@yahoo.com>
2025-03-21 18:53:35 -0400

    redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-	component: Home,
+	loader: () => {
+		throw redirect({ to: '/films' });
+	},
 });
-
-function Home() {
-	return (
-		<div className="p-2">
-			<h3>Welcome Home!!!!!</h3>
-		</div>
-	);
-}
