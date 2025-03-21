f048a61265f2d1a355582592ef05536b30973c09
Eric Hicks <ehicks05@yahoo.com>
2025-03-21 18:46:22 -0400

    preset

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+  },
   tsr: {
     appDirectory: 'src',
   },
